d425e3e71f8faab68ff856b0d8cddb570677241f
Mahnatse-rgb <mfchoung01@gmail.com>
2020-04-14 11:51:06 +0200

    adding attributes to object

--- oop_for_data_science.py
+++ oop_for_data_science.py
@@ -1,1 +1,12 @@
-Class ErrorCalculator():
+class ErrorCalculator:
+
+    def __init__(self, residuals, standardised_residuals, mse, rmse, error):
+
+        self.residuals              =   residuals
+        self.standardised_residuals =   standardised_residuals
+        self.mse                    =   mse
+        self.rmse                   =   rmse
+        self.error                  =   summary
+
+
+    def get_residuals
