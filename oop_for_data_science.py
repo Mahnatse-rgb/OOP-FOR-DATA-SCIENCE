95e64b229a173bf8416d0c160a793703f39ef600
Mahnatse-rgb <mfchoung01@gmail.com>
2020-04-15 09:21:48 +0200

    added an error summary

--- oop_for_data_science.py
+++ oop_for_data_science.py
@@ -2,43 +2,41 @@ class ErrorCalculator:
 
     def __init__(self, y, y_pred):
 
-        self.              =   residuals
-        self. =   standardised_residuals
-        self.                    =   mse
-        self.                   =   rmse
-        self.                 =   summary
+        self.y          =   y       # target
+        self.y_pred     =   y_pred  # prediction of target
 
     import numpy as np
-
-    def size_inspect(y):
-
-        if isinstance(y, )
-
-
 
 
 
     def get_residuals(self):
 
-        residuals =
+        residuals = self.y - self.y_pred
         return residuals
 
     def get_standardised_residuals(self):
 
-        standardised_residuals =
-        return
+        standardised_residuals = self.get_residuals() / (self.get_residuals()/std())
+        return standardised_residuals
 
     def get_mse(self):
 
-        mse =
+        mse = np.square(np.subtract(self.y , self.pred)).mean()
         return mse
 
     def get_rmse(self):
 
-        rmse =
+        rmse = np.sqrt(((self.y_pred - self.y)**2).mean())
         return rmse
 
     def error_summary(self):
 
-        error =
-        return error
+        print('error_summary: ')
+
+        std_resid = self.get_standardised_residuals()
+
+        print(f'average standard residuals: {std_resid.mean()}')
+        print(f'minimum standard residuals: {min(std_resid)}')
+        print(f'maximum standard residuals: {max(std_resid)}')
+        print(f'MSE: {self.get_mse()}')
+        print(f'RSME: {self.get_rmse()}')
